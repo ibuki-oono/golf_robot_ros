7223f1c1b214246c9894ed64fee6456cd28f759d
Ibuki Ono <8326dmc@gmail.com>
2025-11-06 12:20:00 +0900

    change the timeings as needed.

--- golf_robot_control/golf_robot_control/ball_follow_node.py
+++ golf_robot_control/golf_robot_control/ball_follow_node.py
@@ -79,11 +79,11 @@ class BallFollowNode(Node):
             twist.angular.z = 0.0
 
             # If distance < 0.5, stop and then go straight for 3 seconds
-            if distance < 0.5:
+            if distance < 0.36:
                 twist.linear.x = self.straight_speed  # Approach slowly
                 twist.angular.z = 0.0
                 self.cmd_pub.publish(twist)
-                time.sleep(5)  # Small delay to ensure stop
+                time.sleep(6)  # Small delay to ensure stop
                 twist.linear.x = 0.0
                 twist.angular.z = 0.0
                 self.cmd_pub.publish(twist)
